585da154554acfa67b8b8a691fa42b5c64758925
lucas-carl <mail@lucascarl.com>
2017-10-11 20:33:53 +0200

    fix .htaccess & add more routes files

--- api.php
+++ api.php
@@ -11,3 +11,25 @@ $app->get('/files', function (Request $request, Response $response) {
 	$q->execute();
 	return $response->withJson($q->fetchAll(PDO::FETCH_CLASS));
 });
+
+$app->post('/files', function (Request $request, Response $response) {
+	$data = $request->getParsedBody();
+
+	$q = $this->db->prepare('INSERT INTO files (id, title, user_id) VALUES (:id, :title, :user_id)');
+	$q->execute([
+		':id' => '1',
+		':title' => $data['title'],
+		':user_id' => $data['user_id']
+	]);
+
+	return $response->withJson($data);
+});
+
+$app->get('/files/{id}', function (Request $request, Response $response, $args) {
+	$q = $this->db->prepare('SELECT * FROM files WHERE id = :id');
+	$q->execute([
+		':id' => $args['id']
+	]);
+
+	return $response->withJson($q->fetchAll(PDO::FETCH_CLASS));
+});
