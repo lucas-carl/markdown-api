b4994f8c7cd0ab691ba647b5db56e38923e8912e
lucas-carl <mail@lucascarl.com>
2017-10-14 14:54:31 +0200

    add route to update files

--- api.php
+++ api.php
@@ -2,6 +2,9 @@
 use \Psr\Http\Message\ServerRequestInterface as Request;
 use \Psr\Http\Message\ResponseInterface as Response;
 
+header('Access-Control-Allow-Origin: *');
+header('Access-Control-Allow-Methods: GET,POST,PUT,DELETE,OPTIONS');
+
 $app->get('/', function (Request $request, Response $response) {
   return "Markdown Journal API v1.0.0";
 });
@@ -17,7 +20,7 @@ $app->post('/files', function (Request $request, Response $response) {
 
 	$q = $this->db->prepare('INSERT INTO files (id, title, user_id) VALUES (:id, :title, :user_id)');
 	$q->execute([
-		':id' => '1',
+		':id' => $data['id'],
 		':title' => $data['title'],
 		':user_id' => $data['user_id']
 	]);
@@ -31,7 +34,26 @@ $app->get('/files/{id}', function (Request $request, Response $response, $args) 
 		':id' => $args['id']
 	]);
 
-	return $response->withJson($q->fetchAll(PDO::FETCH_CLASS));
+	return $response->withJson($q->fetchAll(PDO::FETCH_CLASS)[0]);
+});
+
+$app->put('/files/{id}', function (Request $request, Response $response, $args) {
+	$data = $request->getParsedBody();
+
+	$q = $this->db->prepare('UPDATE files SET title = :title, content = :content, folder_id = :folder_id WHERE id = :id');
+	$q->execute([
+		':id' => $args['id'],
+		':title' => $data['title'],
+		':content' => $data['content'],
+		':folder_id' => $data['folder_id']
+	]);
+
+	$q = $this->db->prepare('SELECT * FROM files WHERE id = :id');
+	$q->execute([
+		':id' => $args['id']
+	]);
+
+	return $response->withJson($q->fetchAll(PDO::FETCH_CLASS)[0]);
 });
 
 $app->delete('/files/{id}', function (Request $request, Response $response, $args) {
