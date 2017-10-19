5ad94285ddb87f6169461beae391d663c11db5c9
lucas-carl <mail@lucascarl.com>
2017-10-20 00:42:35 +0200

    add token authentication

--- api.php
+++ api.php
@@ -1,30 +1,66 @@
 <?php
-use \Psr\Http\Message\ServerRequestInterface as Request;
-use \Psr\Http\Message\ResponseInterface as Response;
 
 header('Access-Control-Allow-Origin: *');
+header('Access-Control-Allow-Headers: token');
 header('Access-Control-Allow-Methods: GET,POST,PUT,DELETE,OPTIONS');
 
-$app->get('/', function (Request $request, Response $response) {
+$app->add(function ($request, $response, $next) {
+	if ($request->isOptions()) {
+		return $next($requet, $response);
+	}
+
+	$token = $request->getHeaderLine('Token');
+
+	$q = $this->db->prepare('SELECT email, token FROM users WHERE token = :token AND token_expire > :now');
+	$q->execute([
+		':token' => $token,
+		':now' => date('Y-m-d H:i:s')
+	]);
+
+	$user = $q->fetchAll(PDO::FETCH_CLASS)[0];
+
+	if (!$token || !$user) {
+		return $response->withStatus(401);
+	}
+
+	$token_expire = date('Y-m-d H:i:s', strtotime('+8 hours'));
+
+	$u = $this->db->prepare('UPDATE users SET token_expire = :token_expire WHERE email = :email');
+	$u->execute([
+		':email' => $user->email,
+		':token_expire' => $token_expire
+	]);
+
+	return $next($request->withAttribute('user', $user->email), $response);
+});
+
+$app->get('/', function ($request, $response) {
   return "Markdown Journal API v1.0.0";
 });
 
-$app->get('/files', function (Request $request, Response $response) {
-	$q = $this->db->prepare('SELECT * FROM files WHERE not_deleted = 1');
-	$q->execute();
-	return $response->withJson($q->fetchAll(PDO::FETCH_CLASS));
+$app->get('/files', function ($request, $response) {
+	$q = $this->db->prepare('SELECT * FROM files WHERE user_id = :user AND not_deleted = 1');
+	$q->execute([
+		':user' => $request->getAttribute('user')
+	]);
+
+	$files = $q->fetchAll(PDO::FETCH_CLASS);
+
+	if (! $files) {
+		return $response->withStatus(404, 'Not found');
+	}
+
+	return $response->withJson($files);
 });
 
-$app->post('/files', function (Request $request, Response $response) {
-	$data = $request->getParsedBody();
+$app->post('/files', function ($request, $response) {
+	$id = md5(uniqid($request->getAttribute('title') . $request->getAttribute('user'), true));
 
-	$id = md5(uniqid($data['title'] . $data['user_id'], true));
-
-	$q = $this->db->prepare('INSERT INTO files (id, title, user_id, content) VALUES (:id, :title, :user_id, "")');
-	$q->execute([
+	$i = $this->db->prepare('INSERT INTO files (id, title, user_id, content) VALUES (:id, :title, :user, "")');
+	$i->execute([
 		':id' => $id,
-		':title' => $data['title'],
-		':user_id' => $data['user_id']
+		':title' => $request->getAttribute('title'),
+		':user' => $request->getAttribute('user')
 	]);
 
 	$q = $this->db->prepare('SELECT * FROM files WHERE id = :id');
@@ -35,7 +71,32 @@ $app->post('/files', function (Request $request, Response $response) {
 	return $response->withJson($q->fetchAll(PDO::FETCH_CLASS)[0]);
 });
 
-$app->get('/files/{id}', function (Request $request, Response $response, $args) {
+$app->get('/files/{id}', function ($request, $response, $args) {
+	$q = $this->db->prepare('SELECT * FROM files WHERE user_id = :user AND id = :id');
+	$q->execute([
+		':id' => $args['id'],
+		':user' => $request->getAttribute('user')
+	]);
+
+	$file = $q->fetchAll(PDO::FETCH_CLASS)[0];
+
+	if (!$file) {
+		return $response->withStatus(404, 'Not found');
+	}
+
+	return $response->withJson($file);
+});
+
+$app->put('/files/{id}', function ($request, $response, $args) {
+	$u = $this->db->prepare('UPDATE files SET title = :title, content = :content, folder_id = :folder_id WHERE user_id = :user AND id = :id');
+	$u->execute([
+		':id' => $args['id'],
+		':title' => $request->getAttribute('title'),
+		':content' => $request->getAttribute('content'),
+		':folder_id' => $request->getAttribute('folder_id'),
+		':user' => $request->getAttribute('user')
+	]);
+
 	$q = $this->db->prepare('SELECT * FROM files WHERE id = :id');
 	$q->execute([
 		':id' => $args['id']
@@ -44,57 +105,60 @@ $app->get('/files/{id}', function (Request $request, Response $response, $args) 
 	return $response->withJson($q->fetchAll(PDO::FETCH_CLASS)[0]);
 });
 
-$app->put('/files/{id}', function (Request $request, Response $response, $args) {
-	$data = $request->getParsedBody();
-
-	$q = $this->db->prepare('UPDATE files SET title = :title, content = :content, folder_id = :folder_id WHERE id = :id');
-	$q->execute([
+$app->delete('/files/{id}', function ($request, $response, $args) {
+	$u = $this->db->prepare('UPDATE files SET deleted_at = :now, not_deleted = 0 WHERE user_id = :user AND id = :id');
+	$u->execute([
 		':id' => $args['id'],
-		':title' => $data['title'],
-		':content' => $data['content'],
-		':folder_id' => $data['folder_id']
-	]);
-
-	$q = $this->db->prepare('SELECT * FROM files WHERE id = :id');
-	$q->execute([
-		':id' => $args['id']
-	]);
-
-	return $response->withJson($q->fetchAll(PDO::FETCH_CLASS)[0]);
-});
-
-$app->delete('/files/{id}', function (Request $request, Response $response, $args) {
-	$q = $this->db->prepare('UPDATE files SET deleted_at = :now, not_deleted = 0 WHERE id = :id');
-	$q->execute([
-		':id' => $args['id'],
-		':now' => date('Y-m-d H:i:s')
+		':now' => date('Y-m-d H:i:s'),
+		':user' => $request->getAttribute('user')
 	]);
 
 	return $response->withStatus(204);
 });
 
-$app->post('/files/{id}/restore', function (Request $request, Response $response, $args) {
-	$q = $this->db->prepare('UPDATE files SET deleted_at = null, not_deleted = 1 WHERE id = :id');
-	$q->execute([
+$app->post('/files/{id}/restore', function ($request, $response, $args) {
+	$u = $this->db->prepare('UPDATE files SET deleted_at = null, not_deleted = 1 WHERE id = :id');
+	$u->execute([
 		':id' => $args['id']
 	]);
 
 	return $response->withStatus(204);
 });
 
-$app->post('/auth/login', function (Request $request, Response $response) {
-	$data = $request->getParsedBody();
+$app->post('/user/login', function ($request, $response) {
+	$email = $request->getAttribute('email');
 
 	$q = $this->db->prepare('SELECT * FROM users WHERE email = :email');
 	$q->execute([
-		':email' => $data['email']
+		':email' => $email
 	]);
 
 	$user = $q->fetchAll(PDO::FETCH_CLASS)[0];
 
-	if ($user && password_verify($data['password'], $user->password)) {
-		return $response->withJson($user);
+	if ($user && password_verify($request->getAttribute('password'), $user->password)) {
+		$token = bin2hex(openssl_random_pseudo_bytes(8));
+
+		$token_expire = date('Y-m-d H:i:s', strtotime('+8 hours'));
+
+		$u = $this->db->prepare('UPDATE users SET token = :token, token_expire = :token_expire WHERE email = :email');
+		$u->execute([
+			':email' => $email,
+			':token' => $token,
+			':token_expire' => $token_expire
+		]);
+
+		return $response->withJson([ 'email' => $email, 'token' => $token ]);
 	}
 
 	return $response->withStatus(401, 'Bad credentials');
+});
+
+$app->post('/user', function ($request, $response) {
+	$i = $this->db->prepare('INSERT INTO users (email, password) VALUES (:email, :password)');
+	$i->execute([
+		':email' => $request->getAttribute('email'),
+		':password' => password_hash($request->getAttribute('password'), PASSWORD_DEFAULT)
+	]);
+
+	return $response->withStatus(204);
 });
